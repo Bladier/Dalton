89d60a01e2cf83342fbc314f5588df4afa4af0c2
Bladier <misionaellie@gmail.com>
2016-08-23 17:11:35 +0800

    Update

---
+++ Pawnshop/Pawnshop/POS/frmIMD.Designer.vb
@@ -0,0 +1,179 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmIMD
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lvIMD = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader11 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader12 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader13 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader14 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader15 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader16 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnImport = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.lblSearch = New System.Windows.Forms.Label()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lvIMD
+        '
+        Me.lvIMD.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader11, Me.ColumnHeader12, Me.ColumnHeader13, Me.ColumnHeader14, Me.ColumnHeader15, Me.ColumnHeader16})
+        Me.lvIMD.FullRowSelect = True
+        Me.lvIMD.GridLines = True
+        Me.lvIMD.Location = New System.Drawing.Point(12, 71)
+        Me.lvIMD.Name = "lvIMD"
+        Me.lvIMD.Size = New System.Drawing.Size(894, 377)
+        Me.lvIMD.TabIndex = 0
+        Me.lvIMD.UseCompatibleStateImageBehavior = False
+        Me.lvIMD.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ItemCode"
+        Me.ColumnHeader1.Width = 115
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Description"
+        Me.ColumnHeader2.Width = 261
+        '
+        'ColumnHeader11
+        '
+        Me.ColumnHeader11.Text = "UnitOfMeasure"
+        Me.ColumnHeader11.Width = 120
+        '
+        'ColumnHeader12
+        '
+        Me.ColumnHeader12.Text = "Price"
+        Me.ColumnHeader12.Width = 129
+        '
+        'ColumnHeader13
+        '
+        Me.ColumnHeader13.Text = "onHold(Y/N)"
+        Me.ColumnHeader13.Width = 116
+        '
+        'ColumnHeader14
+        '
+        Me.ColumnHeader14.Text = "isInv"
+        Me.ColumnHeader14.Width = 44
+        '
+        'ColumnHeader15
+        '
+        Me.ColumnHeader15.Text = "isSale"
+        Me.ColumnHeader15.Width = 45
+        '
+        'ColumnHeader16
+        '
+        Me.ColumnHeader16.Text = "HasSerial"
+        Me.ColumnHeader16.Width = 59
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnImport)
+        Me.GroupBox1.Controls.Add(Me.txtSearch)
+        Me.GroupBox1.Controls.Add(Me.lblSearch)
+        Me.GroupBox1.Controls.Add(Me.btnSearch)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(894, 62)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'btnImport
+        '
+        Me.btnImport.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnImport.Location = New System.Drawing.Point(801, 25)
+        Me.btnImport.Name = "btnImport"
+        Me.btnImport.Size = New System.Drawing.Size(75, 23)
+        Me.btnImport.TabIndex = 3
+        Me.btnImport.Text = "Import"
+        Me.btnImport.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(80, 26)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(634, 22)
+        Me.txtSearch.TabIndex = 2
+        '
+        'lblSearch
+        '
+        Me.lblSearch.AutoSize = True
+        Me.lblSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSearch.Location = New System.Drawing.Point(23, 28)
+        Me.lblSearch.Name = "lblSearch"
+        Me.lblSearch.Size = New System.Drawing.Size(51, 16)
+        Me.lblSearch.TabIndex = 1
+        Me.lblSearch.Text = "Search"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSearch.Location = New System.Drawing.Point(720, 25)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 0
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'frmIMD
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(916, 460)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lvIMD)
+        Me.Name = "frmIMD"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ITEM MASTER DATA"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lvIMD As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader11 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader12 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader13 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader14 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader15 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader16 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnImport As System.Windows.Forms.Button
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents lblSearch As System.Windows.Forms.Label
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+End Class
---
+++ Pawnshop/Pawnshop/POS/frmIMD.vb
@@ -0,0 +1,148 @@
+﻿Imports Microsoft.Office.Interop
+Public Class frmIMD
+    Const MASTERDATA As String = "\Template\Template-IMD.xlsx"
+
+    Private Sub frmIMD_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub btnImport_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnImport.Click
+     
+        If lvIMD.Items.Count = 0 Then
+
+            Dim ItemMasterData As Excel.Application
+            Dim MaxEntries As Integer
+
+            ItemMasterData = OpenExcel(Application.StartupPath & MASTERDATA)
+            Dim dsIMD As New DataSet
+            dsIMD.Tables.Add(tbl_IMD)
+
+            Dim imdSheet As Excel.Worksheet
+            Dim imdBook As Excel.Workbook
+            Dim dsNewRow As DataRow
+            imdBook = ItemMasterData.Workbooks(1)
+            imdSheet = imdBook.Worksheets(1)
+
+            With imdSheet
+                MaxEntries = .Cells(.Rows.Count, 1).End(Excel.XlDirection.xlUp).row
+            End With
+
+            Console.WriteLine("MaxEntries : " & MaxEntries)
+
+            For rowIdx As Integer = 2 To MaxEntries
+                dsNewRow = dsIMD.Tables(0).NewRow
+                With dsNewRow
+                    .Item("ItemCode") = Trim(imdSheet.Cells(rowIdx, 1).value)
+                    .Item("Description") = Trim(imdSheet.Cells(rowIdx, 2).value)
+                    .Item("UnitOfMeasure") = Trim(imdSheet.Cells(rowIdx, 3).value)
+                    .Item("Price") = Trim(imdSheet.Cells(rowIdx, 4).value)
+                    .Item("onHold(Y/N)") = Trim(imdSheet.Cells(rowIdx, 5).value)
+                    .Item("isInv") = Trim(imdSheet.Cells(rowIdx, 6).value)
+                    .Item("isSale") = Trim(imdSheet.Cells(rowIdx, 7).value)
+                    .Item("HasSerial") = Trim(imdSheet.Cells(rowIdx, 8).value)
+                End With
+                dsIMD.Tables(0).Rows.Add(dsNewRow)
+            Next
+
+            For i As Integer = 0 To dsIMD.Tables(0).Rows.Count - 1
+                Dim listRow As New ListViewItem
+                listRow.Text = dsIMD.Tables(0).Rows(i)(0).ToString()
+                For j As Integer = 1 To dsIMD.Tables(0).Columns.Count - 1
+                    listRow.SubItems.Add(dsIMD.Tables(0).Rows(i)(j).ToString())
+                Next
+                lvIMD.Items.Add(listRow)
+            Next
+
+            CloseExcel(ItemMasterData)
+
+            Console.WriteLine("Database Records: " & dsIMD.Tables(0).Rows.Count)
+        Else
+            MsgBox("Listview contains " & lvIMD.Items.Count & " items")
+            Exit Sub
+        End If
+    End Sub
+
+
+    Private Sub btnSearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSearch.Click
+        lvIMD.MultiSelect = False
+        lvIMD.FullRowSelect = True
+
+        Dim checkInt As Integer = FindItem(lvIMD, txtSearch.Text)
+        If checkInt <> -1 Then
+            lvIMD.Items(checkInt).Selected = True
+            lvIMD.Focus()
+            lvIMD.SelectedItems(0).EnsureVisible()
+        Else
+            MsgBox("Search string not found", MsgBoxStyle.Information)
+        End If
+    End Sub
+
+    Private Function FindItem(ByVal LV As ListView, ByVal TextToFind As String) As Integer
+
+        For i As Integer = 0 To LV.Items.Count - 1
+            If Trim(LV.Items(i).Text) = Trim(TextToFind) Then
+                Return (i)
+            End If
+            For subitem As Integer = 0 To LV.Items(i).SubItems.Count - 1
+                If Trim(LV.Items(i).SubItems(subitem).Text) = Trim(TextToFind) Then
+                    Return (i)
+                End If
+            Next
+        Next
+        ' If not found, then return -1.
+        Return -1
+    End Function
+
+    Private Function tbl_IMD() As DataTable
+        Dim tbl As New DataTable("ITEMMASTERDATA")
+        With tbl.Columns
+            .Add("ItemCode")
+            .Add("Description")
+            .Add("UnitOfMeasure")
+            .Add("Price")
+            .Add("onHold(Y/N)")
+            .Add("isInv")
+            .Add("isSale")
+            .Add("HasSerial")
+        End With
+
+        Return tbl
+    End Function
+
+    Private Function OpenExcel(ByVal src As String) As Excel.Application
+        Dim oXL As New Excel.Application
+        Dim oWB As Excel.Workbook
+
+        oWB = oXL.Workbooks.Open(src)
+
+        Return oXL
+    End Function
+
+    Private Sub CloseExcel(ByVal OpenExcel As Excel.Application)
+        OpenExcel.Quit()
+        OpenExcel = Nothing
+
+        ReleaseObject(OpenExcel)
+    End Sub
+
+    Private Sub ReleaseObject(ByVal obj As Object)
+        Try
+            Dim intRel As Integer = 0
+            Do
+                intRel = System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            Loop While intRel > 0
+            'MsgBox("Final Released obj # " & intRel)
+        Catch ex As Exception
+            'MsgBox("Error releasing object" & ex.ToString)
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+
+   
+
+
+
+End Class
